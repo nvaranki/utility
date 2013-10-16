05d619c7590cbd79503128b6a2b48062499a2d23
Nikolai Varankine <nvaranki@tut.by>
2013-10-16 09:57:44 +0000

    introducing property package

---
+++ src/com/varankin/property/MonitoredListMirror.java
@@ -0,0 +1,50 @@
+package com.varankin.property;
+
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Реализация списка типа {@link MonitoredList}, которая копирует исходный 
+ * MonitoredList. Копирование выполняется посредством назначаемого
+ * {@link PropertyChangeListener}, который использует предоставленный агент 
+ * копирования. Данный список является внешне неизменяемым объектом. 
+ *
+ * @author &copy; 2013 Николай Варанкин
+ */
+public class MonitoredListMirror<E> extends MonitoredList<E>
+{
+    private final MonitoredList<E> SOURCE;
+
+    public MonitoredListMirror( MonitoredList<E> source, Copier<E> агент )
+    {
+        super( new LinkedList<E>() );
+        SOURCE = source;
+        агент.copy( SOURCE, (List)this );
+        source.наблюдатели().add( new PropertyChangeListenerImpl( агент ) );
+    }
+
+    public interface Copier<E>
+    {
+        void copy( List<E> source, List<E> target );
+    }
+    
+    private class PropertyChangeListenerImpl implements PropertyChangeListener
+    {
+        private final Copier<E> АГЕНТ;
+
+        PropertyChangeListenerImpl( Copier<E> агент )
+        {
+            АГЕНТ = агент;
+        }
+        
+        @Override
+        public void propertyChange( PropertyChangeEvent evt )
+        {
+            АГЕНТ.copy( SOURCE, MonitoredListMirror.this );
+        }
+
+    }
+    
+}
